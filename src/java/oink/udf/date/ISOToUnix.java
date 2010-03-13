e4ade4876a43363748e708b4797b0699de32ca33
criccomi <criccomi@criccomi-md.linkedin.biz>
2010-03-12 17:48:27 -0800

    adding more jars moving date into udf dir added string added sqrt

---
+++ src/java/oink/udf/date/ISOToUnix.java
@@ -0,0 +1,18 @@
+package oink.udf.date;
+
+import java.io.IOException;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+public class ISOToUnix extends EvalFunc<String> {
+	public String exec(Tuple input) throws IOException {
+		// Set the time to default or the output is in UTC
+		DateTimeZone.setDefault(DateTimeZone.UTC);
+		DateTime result = new DateTime(input.get(0).toString());
+
+		return Long.toString(result.getMillis());
+	}
+}
---
+++ src/java/oink/udf/date/toWeek.java
@@ -0,0 +1,22 @@
+package oink.udf.date;
+
+import java.io.IOException;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+public class toWeek extends EvalFunc<String> {
+	public String exec(Tuple input) throws IOException {
+		// Set the time to default or the output is in UTC
+		DateTimeZone.setDefault(DateTimeZone.UTC);
+		DateTime dt = new DateTime((String) input.get(0).toString());
+
+		// Set the the day to 1, and the hour, minute, second and milliseconds
+		// to 0
+		DateTime result = dt.dayOfWeek().setCopy(1).hourOfDay().setCopy(0).minuteOfHour().setCopy(0).secondOfMinute().setCopy(0).millisOfSecond().setCopy(0);
+
+		return result.toString();
+	}
+}
